1c4f8ea87a61b156ce4b33a54789ecbbd64e96bd
roy wahome <roy.njuguna@student.moringaschool.com>
2024-08-20 14:06:31 +0300

    the fainal commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import BotPage from "./Pages/BotPage";
+import "./App.css";
+
+function App() { 
+  
+    return (
+      <div className="App">
+        <BotPage />
+      </div>
+    );
+  }
+
+
+export default App;
